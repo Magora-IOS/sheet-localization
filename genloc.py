4cb9123158484a9d16da76583908b741febb319c
Michael Kapelko <kornerr@gmail.com>
2017-07-10 14:49:27 +0700

    Verify spreadsheet and sheet presence

--- genloc.py
+++ genloc.py
@@ -17,6 +17,28 @@ scope = ["https://spreadsheets.google.com/feeds"]
 credentials = ServiceAccountCredentials.from_json_keyfile_name(credentialsFileName, scope)
 client = gspread.authorize(credentials)
 
-spreadsheet = client.open(documentName)
-sheet1 = spreadsheet.sheet1
+# Get spreadsheet.
+spreadsheet = None
+try:
+    spreadsheet = client.open(documentName)
+except:
+    msg = """
+ERROR: Could not find spreadsheet named '{0}'.
+Make sure:
+* it exists
+* you have added the service account email as the document editor in document sharing settings
+""".format(documentName)
+    print(msg)
+    raise
+
+# Get "SRC" page.
+srcPage = None
+try:
+    srcPage = spreadsheet.worksheet("SRC")
+except:
+    msg = """
+ERROR: Could not find sheet named 'SRC'. Make sure it exists
+"""
+    print(msg)
+    raise
 
